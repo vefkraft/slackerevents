e6fab27272093a643b54d1e20e2ca29e5fe3d933
Miau <165878715+bema96@users.noreply.github.com>
2025-06-16 14:39:37 +0000

    test font

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -8,7 +8,14 @@ import "@/styles/variables.css";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Squada+One&display=swap" rel="stylesheet" />
+      </head>
+      <body
+       className="">{children}
+      </body>
     </html>
   );
 }
